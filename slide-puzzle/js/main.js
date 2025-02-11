1a977bb9207459495288ad31c74f5b1ebc312f91
fujtak <fujtak.eng23@gmail.com>
2025-02-11 23:23:22 +0900

    位置変えの仕組みを用意

--- slide-puzzle/js/main.js
+++ slide-puzzle/js/main.js
@@ -12,13 +12,27 @@ const generateTile = () => {
       const tile = document.createElement('div')
       const index = i * rowCount + j
       tile.classList.add('tile')
-      tile.textContent = (index === 0) ? '' : index
+      tile.dataset.tile = true
       tile.dataset.index = index
       tile.dataset.value = index
+      tile.textContent = (index === 0) ? '' : index
       row.appendChild(tile)
     }
     root.appendChild(row)
   }
 }
 
+const swap = (i, j) => {
+  const tiles = document.querySelectorAll('[data-tile]')
+  const tileI = tiles[i]
+  const tileJ = tiles[j]
+  const tmpValue = tileI.dataset.value
+  const tmpTextContent = tileI.textContent
+  tileI.dataset.value = tileJ.dataset.value
+  tileJ.dataset.value = tmpValue
+  tileI.textContent = tileJ.textContent
+  tileJ.textContent = tmpTextContent
+}
+
 generateTile()
+swap(0, 6)
