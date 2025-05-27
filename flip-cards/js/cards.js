d8ef23942c222369faa242fd5e599e0cfa91023f
fujtak <fujtak.eng23@gmail.com>
2025-05-27 22:41:09 +0900

    神経衰弱を作成中

--- flip-cards/js/cards.js
+++ flip-cards/js/cards.js
@@ -1,2 +1,29 @@
 'use strict'
 
+const NUMBER_MAX = 20
+
+const generateNumbers = () => {
+  const numbers = []
+  for(let i = 0; i < NUMBER_MAX; ++i) {
+    for(let j = 0; j < 2; ++j) {
+      numbers.push(i + 1)
+    }
+  }
+  return numbers
+}
+
+
+
+const generateDOM = (numbers) => {
+  const root = document.querySelector('.card-container')
+  for(const number of numbers) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.textContent = number
+    root.appendChild(card)
+  }
+}
+
+const numbers = generateNumbers()
+const numbersShuffled = 
+generateDOM(numbers)
